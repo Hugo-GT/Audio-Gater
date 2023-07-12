af009b40ee21b3e9c9ae34debe8c57af3f6af3b6
Hugo-GT <hugo.ga.t@hotmail.com>
2023-07-12 01:43:00 -0400

    First push

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,216 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent() : state(Stopped)
+{
+	//addAndMakeVisible(&testBtn);
+	//testBtn.setButtonText("Test");
+	//testBtn.onClick = [this]() { transportSlider.setValue(20.0); };
+
+	// Make sure you set the size of the component after
+	// you add any child components.
+	addAndMakeVisible(&openBtn);
+	openBtn.setButtonText("Open...");
+	openBtn.onClick = [this]() { openBtnClicked(); };
+	openBtn.setColour(juce::TextButton::buttonColourId, juce::Colours::blueviolet);
+
+	addAndMakeVisible(&playBtn);
+	playBtn.setButtonText("Play");
+	playBtn.onClick = [this]() { playBtnClicked(); };
+	playBtn.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
+	playBtn.setEnabled(false);
+
+	addAndMakeVisible(&stopBtn);
+	stopBtn.setButtonText("Stop");
+	stopBtn.onClick = [this]() { stopBtnClicked(); };
+	stopBtn.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
+	stopBtn.setEnabled(false);
+
+	setSize(300, 200);
+
+	formatManager.registerBasicFormats();       // [1]
+	transportSource.addChangeListener(this);   // [2]
+
+	setAudioChannels(2, 2);
+
+}
+
+MainComponent::~MainComponent()
+{
+	// This shuts down the audio device and clears the audio source.
+	shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+	transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+	if (readerSource.get() == nullptr)
+	{
+		bufferToFill.clearActiveBufferRegion();
+		return;
+	}
+	transportSource.getNextAudioBlock(bufferToFill);
+}
+
+void MainComponent::releaseResources()
+{
+	// This will be called when the audio device stops, or when it is being
+	// restarted due to a setting change.
+	// For more details, see the help for AudioProcessor::releaseResources()
+
+	transportSource.releaseResources();
+}
+
+//==============================================================================
+void MainComponent::paint(juce::Graphics& g)
+{
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
+	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+
+	// You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+	// This is called when the MainContentComponent is resized.
+	// If you add any child components, this is where you should
+	// update their positions.
+
+	int vSpacing = getHeight() * 0.04;
+	int hSpacing = getWidth() * 0.04;
+
+	juce::Grid grid;
+
+	using Track = juce::Grid::TrackInfo;
+	using Fr = juce::Grid::Fr;
+
+	grid.templateRows = {
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1))
+	};
+	grid.templateColumns = {
+		Track(Fr(1)),
+		Track(Fr(1)),
+		Track(Fr(1))
+	};
+
+	grid.columnGap.pixels = hSpacing;
+	grid.rowGap.pixels = vSpacing;
+
+	grid.items = {
+		juce::GridItem(openBtn).withArea(3,2),
+		juce::GridItem(playBtn).withArea(4,2),
+		juce::GridItem(stopBtn).withArea(5,2),
+	};
+
+	grid.performLayout(getLocalBounds().reduced(vSpacing));
+}
+
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+	if (source == &transportSource)
+	{
+		if (transportSource.isPlaying())
+			changeState(Playing);
+		else
+			changeState(Stopped);
+	}
+}
+
+double MainComponent::getSongLengthInSeconds()
+{
+	return readerSource != nullptr ? readerSource->getAudioFormatReader()->lengthInSamples / readerSource->getAudioFormatReader()->sampleRate : 0.0;
+}
+
+double MainComponent::getCurrentPositionInSeconds()
+{
+	return transportSource.isPlaying() ? transportSource.getCurrentPosition() : 0.0;
+}
+
+juce::String MainComponent::formatTime(int seconds)
+{
+	int minutes = seconds / 60;
+	int remainingSeconds = seconds % 60;
+
+	juce::String formattedTime;
+	formattedTime << juce::String(minutes).paddedLeft('0', 2) << ":" << juce::String(remainingSeconds).paddedLeft('0', 2);
+
+	return formattedTime;
+}
+
+void MainComponent::openBtnClicked()
+{
+	chooser = std::make_unique<juce::FileChooser>("Select a Wave file to play...",
+		juce::File{},
+		"*.wav, *.mp3");                     // [7]
+
+	auto chooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
+
+	chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)     // [8]
+		{
+			juce::File file = fc.getResult();
+
+			if (file != juce::File{})                                                // [9]
+			{
+				auto* reader = formatManager.createReaderFor(file);
+
+				if (reader != nullptr)
+				{
+					auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);   // [11]
+					transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);       // [12]
+					playBtn.setEnabled(true);                                                      // [13]
+					readerSource.reset(newSource.release());                                          // [14]
+				}
+			}
+		});
+}
+
+void MainComponent::playBtnClicked()
+{
+	changeState(Starting);
+}
+
+void MainComponent::stopBtnClicked()
+{
+	changeState(Stopping);
+}
+
+void MainComponent::changeState(TransportState newState)
+{
+	if (state != newState)
+	{
+		state = newState;
+
+		switch (state)
+		{
+		case Stopped:                           // [3]
+			stopBtn.setEnabled(false);
+			playBtn.setEnabled(true);
+			transportSource.setPosition(0.0);
+			break;
+
+		case Starting:                          // [4]
+			playBtn.setEnabled(false);
+			transportSource.start();
+			break;
+
+		case Playing:                           // [5]
+			stopBtn.setEnabled(true);
+			break;
+
+		case Stopping:                          // [6]
+			transportSource.stop();
+			break;
+		}
+	}
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+	This component lives inside our window, and this is where you should put all
+	your controls and content.
+*/
+class MainComponent : public juce::AudioAppComponent, public juce::ChangeListener
+{
+public:
+	//==============================================================================
+	MainComponent();
+	~MainComponent() override;
+
+	//==============================================================================
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override; // How can I see from where this function gets called (theres like 30 possibilities)
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+	void releaseResources() override;
+
+	//==============================================================================
+	void paint(juce::Graphics& g) override;
+	void resized() override;
+
+private:
+	//==============================================================================
+	// Your private member variables go here...
+	enum TransportState
+	{
+		Stopped,
+		Starting,
+		Playing,
+		Stopping
+	};
+
+	void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+	double getSongLengthInSeconds();
+	double getCurrentPositionInSeconds();
+	juce::String formatTime(int seconds);
+
+	juce::TextButton openBtn;
+	juce::TextButton playBtn;
+	juce::TextButton stopBtn;
+
+	juce::AudioFormatManager formatManager;
+	std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+	juce::AudioTransportSource transportSource;
+
+	std::unique_ptr<juce::FileChooser> chooser;
+
+	void openBtnClicked();
+	void playBtnClicked();
+	void stopBtnClicked();
+	void changeState(TransportState newState);
+
+	TransportState state;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
+
